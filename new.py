6f4377a40c16066c9393cb08728a1758c014e4aa
Oseyenbhin Sunday Osemeahon <ose.sunday@gmail.com>
2025-08-25 17:54:51 -0400

    my new commit

---
+++ new.py
@@ -0,0 +1,13 @@
+variable1=20
+variable2="20"
+is_sunny = True
+is_raining = False
+print()
+
+#list
+developers=["richard", "ose", "helen"]
+accounting=["micheal", "john", "lucas", 2312]
+print(developers)
+
+#access via index
+print(developers[2])
