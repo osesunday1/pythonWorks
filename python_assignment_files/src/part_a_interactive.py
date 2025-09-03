b3c9a2ac2e08bda85eaf9306a1757fd9c412c35c
osesunday1 <ose.sunday@gmail.com>
2025-09-03 15:23:59 -0400

    send

--- python_assignment_files/src/part_a_interactive.py
+++
@@ -1,9 +0,0 @@
-toppings = ['mushrooms', 'green peppers', 'extra cheese', 'pepperoni']
-for topping in toppings:
-    if topping == 'green peppers':
-        print("Sorry, we are out of green peppers right now.")
-    elif topping == 'pepperoni':               # ← new block
-        print("Adding pepperoni (fan favorite)!")
-    else:
-        print(f"Adding {topping}.")
-print("Finished making your pizza!")
