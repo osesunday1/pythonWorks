994762369f63979b4443309f157c801c73bdfc22
Helen Oni (toh-1) <tosin.oni.engineer@gmail.com>
2025-09-04 13:21:28 -0400

    Import assignment files from Downloads

---
+++ python_assignment_files/src/part_c.py
@@ -0,0 +1,13 @@
+
+# Part C: Using multiple lists (available vs requested)
+
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for topping in requested_toppings:
+    if topping in available_toppings:
+        print(f"Adding {topping}.")
+    else:
+        print(f"Sorry, we don't have {topping}.")
+
+print("Finished making your pizza!")
